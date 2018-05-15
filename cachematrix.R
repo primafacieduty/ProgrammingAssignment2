dbb29e39c3473e3a7011e841b14773194c419d41
Leplace's Demon <primafacieduty1930@gmail.com>
2018-05-15 10:42:20 -0400

    Finalized 'cachematrix.R' file for grading

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix algebra computations, such as the inverse of a matrix are often
+## expensive.  The functions below make use of cache memory to compute
+## and store ain inverse matrix
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(x = matrix()) { # defines the argument as a matrix
+    inverse <- NULL # initalize inverse as NULL
+    set <- function(y){ # define set function; assigns new value of matrix in parent environment
+        x <<- y # value of matrix in parent environment
+        inverse <<- NULL # if a matrix already exists reset inverse to NULL
+    }
+    get <- function() x # define 'get' - returns value of matrix argument
+    setinv <- function(CompleteMatrix) inverse <<- CompleteMatrix # defines value of 'inverse' in parent environment
+    getinv <- function() inverse # retrieves the value of inverse
+    list(set = set, get = get, setinv = setinv, getinv = getinv) # creates a list for the functions
 }
 
 
-## Write a short comment describing this function
+## This functions takes the output of "makeCacheMatrix"
+## Function returns the inverse matrix
+## If inverse already stored in cache function retrieves the existing matrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { # argument of function is the output of makeCacheMatrix
+    inv <- x$getinv() # calls value of function getinv
+    if(!is.null(inv)){ # if inv is not NULL then function returns a message and the cached matrix
+        message("Returning cached data")
+        return(inv)
+    }
+    data <- x$get() # if inv is NULL then function retrieves orignial matrix
+    inv <- solve(data) # inverts the matrix; for solve function to work matrix must be square
+    x$setinv (inv) # sets the inverse matrix into cache
+    inv # Return a matrix that is the inverse of 'x'
 }
